a32f35d4f15de0dab2610b9d58206f0c3b2d8275
dimas sudjito <dimassudjito@localhost.localdomain>
2021-03-07 21:16:13 -0700

    add register page

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 
@@ -9,8 +10,32 @@ app.set('view engine', 'hbs');
 app.use(express.urlencoded({ extended: true }));
 app.use('/static', express.static('public'));
 
-// @TODO add auth middleware
-// @TODO add registration page
+app.use(
+require('express-session')({
+    secret: process.env.APP_SECRET,
+    resave: true,
+    saveUninitialized: false
+})
+);
+
+const { ExpressOIDC } = require('@okta/oidc-middleware');
+const oidc = new ExpressOIDC({
+appBaseUrl: process.env.HOST_URL,
+issuer: `${process.env.OKTA_ORG_URL}/oauth2/default`,
+client_id: process.env.OKTA_CLIENT_ID,
+client_secret: process.env.OKTA_CLIENT_SECRET,
+redirect_uri: `${process.env.HOST_URL}/callback`,
+scope: 'openid profile',
+routes: {
+    loginCallback: {
+    path: '/callback'
+    },
+}
+});
+
+app.use(oidc.router);
+
+app.use('/register', require('./routes/register'));
 
 app.use('/', require('./routes/index'));
 
